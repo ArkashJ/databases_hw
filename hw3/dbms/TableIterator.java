a87f99c02a1161f368cd2f71d32c43a40e5e3c4f
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2024-03-24 16:14:02 -0400

    bug fix

--- hw3/dbms/TableIterator.java
+++ hw3/dbms/TableIterator.java
@@ -217,14 +217,13 @@ public class TableIterator {
          * PS 3: replace the following return statement with your 
          * implementation of the rest of this method.
          */
-
         RowInput keyIn = new RowInput(this.key.getData());
         RowInput valueIn = new RowInput(this.value.getData()); 
         // calculate offset to go to using n
         // if n = 1, offset = 2, if n = 4, offset = 2*4
         // Once calculated, read the offset value. Also check the column type
         // if it is VARCHAR, iterate until a non-null offset is found and subtract it to get the necessary length
-        int offset = 2*colIndex;
+        short offset = 2*colIndex;
         if (col.isPrimaryKey()) {
             return this.readValue(keyIn, col, offset);
         }
@@ -243,8 +242,13 @@ public class TableIterator {
                 case Column.REAL:
                     return in.readDoubleAtOffset(offset);
                 case Column.CHAR:
-                    return in.readShortAtOffset(offset);
+                    return in.readBytesAtOffset(offset, col.getLength());
                 case Column.VARCHAR:
+                    // if the column is a primary key, then we read from keyoffset
+                    if (col.isPrimaryKey()) {
+                        return in.readBytesAtOffset(offset, this.key.getSize());
+                    }
+
                     //WARN: System.out.println("Reading varchar at offset " + offset + " with length " + in.readShortAtOffset(offset));
                     short len = in.readShortAtOffset(offset);
                     // for zero length varchar return empty string
@@ -256,7 +260,7 @@ public class TableIterator {
                     }
                     // find the next non null offset then we can calculate the length of the varchar, then we can read the varchar from the offset
                     int forwardOffset = offset+2; 
-                    while (forwardOffset == -1) {
+                    while (forwardOffset == -1 || forwardOffset == -2) {
                         forwardOffset+= 2;
                     }
                     
